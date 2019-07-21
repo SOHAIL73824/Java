8d3909fe1bc30df956fa81ef479cf5eb4e47b090
Sohail Shareef <sohailshareefhyd@gmail.com>
2019-07-21 16:51:10 -0500

    Update TestReflection.java

--- javaAdvance/Day4/Ass32_ReflectionAPI/TestReflection.java
+++ javaAdvance/Day4/Ass32_ReflectionAPI/TestReflection.java
@@ -3,31 +3,30 @@ import java.lang.*;
 import java.lang.reflect.*;
 import java.util.Calendar;
 public class TestReflection {
-public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+public static void main(String[] args) {
 	
 try {
-	Class classObj = Class.forName("javaAdvance.Day4.Ass32_ReflectionAPI.Employee");
+	Class classObj = Employee.class;//Class.forName("javaAdvance.Day4.Ass32_ReflectionAPI.Employee");
 	
 	System.out.println("Class Name: "+classObj.getName());
 
 	System.out.println("Access Modifier of Class: " +Modifier.toString(classObj.getModifiers())); //prints "
 	
-	Employee privateObject = new Employee(1);
-
-	Field privateStringField = Employee.class.
-	            getDeclaredField("privateString");
-
-	privateStringField.setAccessible(true);
-
-	String fieldValue = (String) privateStringField.get(privateObject);
-	System.out.println("fieldValue = " + fieldValue);
-
-
 	
-} catch (ClassNotFoundException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}catch (Exception e) {
+	Field[] field = classObj.getDeclaredFields();
+	
+	for(Field f: field) {
+		
+		System.out.println("variable: "+ f.getName());
+		System.out.println("Variable Types: "+f.getType());
+		
+	}
+	
+	
+
+
+}	
+catch (Exception e) {
 	// TODO Auto-generated catch block
 	System.out.println("Exception: "+e.getMessage());
 }
